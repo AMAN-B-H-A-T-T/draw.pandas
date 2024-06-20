c403533c61c36aca98fa87222156dc2155ea5dfb
AMAN-B-H-A-T-T <22csama003@ldce.ac.in>
2024-06-20 20:14:53 +0530

    setup cors policy

--- index.js
+++ index.js
@@ -9,18 +9,38 @@ const PORT = 4000
 const app = express()
 const path = require('path')
 const Server = http.createServer(app)
+
+const allowedOrigins = ['http://localhost:4000', 'https://draw-pandas.onrender.com'];
+
 const io = socketIo(Server,{
-    cors: {
-      origin: 'http://localhost:3000', // React app URL
-      methods: ['GET', 'POST'],
-      allowedHeaders: ['Content-Type'],
-      credentials: true
-    }
+  cors: {
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true
+  }
   })
 try{
     
     setIoObject(io)
-    app.use(cors())
+    app.use(cors({
+      origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['Content-Type'],
+      credentials: true
+    }));
     connect_db()
     app.use(bodyParser.json())
     app.use("/api/manage",require('./routes/player.routes'))
@@ -35,5 +55,5 @@ catch(error){
 
 
 Server.listen(PORT,(req,res)=>{
-    console.log("http://localhost:4000")
+    console.log(`Server is running on port 4000`)
 })
